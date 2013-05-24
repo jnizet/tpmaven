5d565b98136dc89bbfa7fabf768ecb561d93055d
JB Nizet <jnizet@gmail.com>
2013-05-24 18:27:52 +0200

    initial commit

---
+++ src/main/java/com/ninja_squad/App.java
@@ -0,0 +1,13 @@
+package com.ninja_squad;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/ninja_squad/AppTest.java
@@ -0,0 +1,18 @@
+package com.ninja_squad;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+   
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
